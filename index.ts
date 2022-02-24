ca94ae21dc119d636c577d92150d224928619c43
mirko <50323497+mirkoBesagno@users.noreply.github.com>
2022-02-24 17:16:04 +0100

    WIP

--- index.ts
+++ index.ts
@@ -2,25 +2,90 @@
 
 type operator = '<' | '>' | '==' | '<=' | '>=' | '!=';
 
-const or = function (variabiliConfrontabile: any, params: operator) {
+export const or = function (variabiliConfrontabile: any, params: operator) {
     return function (...variabiliDaConfrontare: any) {
         for (const y of variabiliDaConfrontare) {
-        switch (params) {
-            case '==':
-                if (variabiliConfrontabile == variabiliDaConfrontare) {
-                    return true;
-                }
-                break;
-            default:
-                break;
+            switch (params) {
+                case '==':
+                    if (variabiliConfrontabile == y) {
+                        return true;
+                    }
+                    break;
+                case "!=":
+                    if (variabiliConfrontabile != y) {
+                        return true;
+                    }
+                    break;
+                case "<":
+                    break;
+                    if (variabiliConfrontabile < y) {
+                        return true;
+                    }
+                case "<=":
+                    if (variabiliConfrontabile <= y) {
+                        return true;
+                    }
+                    break;
+                case ">":
+                    if (variabiliConfrontabile > y) {
+                        return true;
+                    }
+                    break;
+                case ">=":
+                    if (variabiliConfrontabile >= y) {
+                        return true;
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
+        return false;
     }
-    return false;
+};
+export const and = function (variabiliConfrontabile: any, params: operator) {
+    return function (...variabiliDaConfrontare: any) {
+        for (const y of variabiliDaConfrontare) {
+            switch (params) {
+                case '==':
+                    if (!(variabiliConfrontabile == y)) {
+                        return false;
+                    }
+                    break;
+                case "!=":
+                    if (!(variabiliConfrontabile != y)) {
+                        return false;
+                    }
+                    break;
+                case "<":
+                    if (!(variabiliConfrontabile < y)) {
+                        return false;
+                    }
+                    break;
+                case "<=":
+                    if (!(variabiliConfrontabile <= y)) {
+                        return false;
+                    }
+                    break;
+                case ">":
+                    if (!(variabiliConfrontabile > y)) {
+                        return false;
+                    }
+                    break;
+                case ">=":
+                    if (!(variabiliConfrontabile >= y)) {
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return true;
     }
-    
-}
+};
+/* console.log( or(true, '==')( false, false, false ));
 
-or(true, '==')(
-    '','',''
-    );
+console.log(''); */
+
 
