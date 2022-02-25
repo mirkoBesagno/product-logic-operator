73c67ddf65f76ac4739ff39478fef5c8bfe71542
mirko <50323497+mirkoBesagno@users.noreply.github.com>
2022-02-25 09:56:07 +0100

    WIP

--- index.ts
+++ index.ts
@@ -2,90 +2,148 @@
 
 type operator = '<' | '>' | '==' | '<=' | '>=' | '!=';
 
-export const or = function (variabiliConfrontabile: any, params: operator) {
+type howToHandleError = 'error' | 'try' | 'return' | "skip";
+
+
+export const or = function (variabiliConfrontabile: any, params: operator, howToHandleError?: howToHandleError) {
     return function (...variabiliDaConfrontare: any) {
         for (const y of variabiliDaConfrontare) {
-            switch (params) {
-                case '==':
-                    if (variabiliConfrontabile == y) {
-                        return true;
-                    }
-                    break;
-                case "!=":
-                    if (variabiliConfrontabile != y) {
-                        return true;
-                    }
-                    break;
-                case "<":
-                    break;
-                    if (variabiliConfrontabile < y) {
-                        return true;
-                    }
-                case "<=":
-                    if (variabiliConfrontabile <= y) {
-                        return true;
-                    }
-                    break;
-                case ">":
-                    if (variabiliConfrontabile > y) {
-                        return true;
-                    }
-                    break;
-                case ">=":
-                    if (variabiliConfrontabile >= y) {
-                        return true;
-                    }
-                    break;
-                default:
-                    break;
+            var salta = false;
+            var tmpY = y;
+            if (typeof y == 'function') {
+                tmpY = y();
+            }
+            var tmpVariabiliConfrontabile = variabiliConfrontabile;
+            if(typeof variabiliConfrontabile == 'function') {
+                tmpVariabiliConfrontabile = variabiliConfrontabile();
+            }
+            if (typeof tmpVariabiliConfrontabile != typeof tmpY) {
+                if (howToHandleError == 'error' || howToHandleError == undefined) {
+                    throw new Error("Attenzione tipi differenti");
+                }
+                else if (howToHandleError == 'return') {
+                    return false;
+                } else if (howToHandleError == 'skip') {
+                    salta = true;
+                }
+                else if (howToHandleError == 'try') {
+                    salta = false;
+                }
+            }
+            if (salta == false) {
+                switch (params) {
+                    case '==':
+                        if (tmpVariabiliConfrontabile == tmpY) {
+                            return true;
+                        }
+                        break;
+                    case "!=":
+                        if (tmpVariabiliConfrontabile != tmpY) {
+                            return true;
+                        }
+                        break;
+                    case "<":
+                        if (tmpVariabiliConfrontabile < tmpY) {
+                            return true;
+                        }
+                        break;
+                    case "<=":
+                        if (tmpVariabiliConfrontabile <= tmpY) {
+                            return true;
+                        }
+                        break;
+                    case ">":
+                        if (tmpVariabiliConfrontabile > tmpY) {
+                            return true;
+                        }
+                        break;
+                    case ">=":
+                        if (tmpVariabiliConfrontabile >= tmpY) {
+                            return true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         return false;
     }
 };
-export const and = function (variabiliConfrontabile: any, params: operator) {
+export const and = function (variabiliConfrontabile: any, params: operator, howToHandleError?: howToHandleError) {
     return function (...variabiliDaConfrontare: any) {
         for (const y of variabiliDaConfrontare) {
-            switch (params) {
-                case '==':
-                    if (!(variabiliConfrontabile == y)) {
-                        return false;
-                    }
-                    break;
-                case "!=":
-                    if (!(variabiliConfrontabile != y)) {
-                        return false;
-                    }
-                    break;
-                case "<":
-                    if (!(variabiliConfrontabile < y)) {
-                        return false;
-                    }
-                    break;
-                case "<=":
-                    if (!(variabiliConfrontabile <= y)) {
-                        return false;
-                    }
-                    break;
-                case ">":
-                    if (!(variabiliConfrontabile > y)) {
-                        return false;
-                    }
-                    break;
-                case ">=":
-                    if (!(variabiliConfrontabile >= y)) {
-                        return false;
-                    }
-                    break;
-                default:
-                    break;
+            var salta = false;
+            var tmpY = y;
+            if (typeof y == 'function') {
+                tmpY = y();
+            }
+            var tmpVariabiliConfrontabile = variabiliConfrontabile;
+            if(typeof variabiliConfrontabile == 'function') {
+                tmpVariabiliConfrontabile = variabiliConfrontabile();
+            }
+            if (typeof tmpVariabiliConfrontabile != typeof tmpY) {
+                if (howToHandleError == 'error' || howToHandleError == undefined) {
+                    throw new Error("Attenzione tipi differenti");
+                }
+                else if (howToHandleError == 'return') {
+                    return false;
+                } else if (howToHandleError == 'skip') {
+                    salta = true;
+                }
+                else if (howToHandleError == 'try') {
+                    salta = false;
+                }
+            }
+            if (salta == false) {
+                switch (params) {
+                    case '==':
+                        if (!(tmpVariabiliConfrontabile == tmpY)) {
+                            return false;
+                        }
+                        break;
+                    case "!=":
+                        if (!(tmpVariabiliConfrontabile != tmpY)) {
+                            return false;
+                        }
+                        break;
+                    case "<":
+                        if (!(tmpVariabiliConfrontabile < tmpY)) {
+                            return false;
+                        }
+                        break;
+                    case "<=":
+                        if (!(tmpVariabiliConfrontabile <= tmpY)) {
+                            return false;
+                        }
+                        break;
+                    case ">":
+                        if (!(tmpVariabiliConfrontabile > tmpY)) {
+                            return false;
+                        }
+                        break;
+                    case ">=":
+                        if (!(tmpVariabiliConfrontabile >= tmpY)) {
+                            return false;
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
         return true;
     }
 };
-/* console.log( or(true, '==')( false, false, false ));
+try {
 
-console.log(''); */
+    const tmp = or(() => { return 'ciao'; }, '==')('kk', () => { return 'ciao'; }, 'hahaha');
+    console.log(tmp);
+
+    console.log('');
 
 
+} catch (error) {
+    console.log(error);
+
+}
